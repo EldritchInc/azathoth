d0020cc036551b6a9bcf50de1c72e50a082ea7c6
Oren Montano <orelm23@gmail.com>
2026-08-05 23:52:26 -0400

    feat(workflows): generate workflow candidates

--- src/azathoth/workflows/__init__.py
+++ src/azathoth/workflows/__init__.py
@@ -1,4 +1,8 @@
 from azathoth.workflows.candidate import WorkflowCandidate
+from azathoth.workflows.generation import (
+    WorkflowGenerationError,
+    generate_workflow_candidate,
+)
 from azathoth.workflows.models import (
     WorkflowMetadata,
     WorkflowSpecification,
@@ -9,7 +13,9 @@ from azathoth.workflows.steps import (
 
 __all__ = [
     "WorkflowCandidate",
+    "WorkflowGenerationError",
     "WorkflowMetadata",
     "WorkflowSpecification",
     "WorkflowStepSpecification",
+    "generate_workflow_candidate",
 ]
