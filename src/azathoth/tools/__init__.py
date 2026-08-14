768dc21259c5c30d8e869408cabe023fb75d8cad
Oren Montano <orelm23@gmail.com>
2026-08-14 14:08:13 -0400

    feat(tools): add tool contracts

---
+++ src/azathoth/tools/__init__.py
@@ -0,0 +1,13 @@
+"""Durable tool definitions and abstractions."""
+
+from azathoth.tools.definition import (
+    ToolDefinition,
+    ToolInputSchema,
+    ToolOutputSchema,
+)
+
+__all__ = [
+    "ToolDefinition",
+    "ToolInputSchema",
+    "ToolOutputSchema",
+]
